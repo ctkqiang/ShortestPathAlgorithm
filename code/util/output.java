43454275e1b94598932f6b870d1dde651b798f76
John Melody Me <Johnmelodyme@yandex.com>
2020-09-15 14:59:21 +0800

    #1

--- code/util/output.java
+++ code/util/output.java
@@ -1,6 +1,5 @@
 package code.util;
 
-import code.minimum_distance;
 import code.value;
 
 public class output {
@@ -8,11 +7,12 @@ public class output {
     static public int K = value.V;
 
     public static void solution(int dist[], int n) {
-        System.out.println("Vertex   Distance from Source");
+        System.out.println("Vertex Distance from Source");
 
         int i;
 
-        for (i = 0; i < K; i++)
+        for (i = 0; i < K; i++) {
             System.out.println(i + " tt " + dist[i]);
+        }
     }
 }
